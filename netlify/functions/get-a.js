b1076b3a9b2a1e747d082fbd3067d3c989cb82e8
ell1zv4 <99562868+ell1zv4@users.noreply.github.com>
2024-10-21 21:05:46 +0200

    Update get-a.js

--- netlify/functions/get-a.js
+++ netlify/functions/get-a.js
@@ -1,7 +1,7 @@
-exports.handler = async (event) => {
-  const url = process.env.PRIVATE_URL;
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ url }), 
-  };
+exports.handler = async function(event, context) {
+    const url = process.env.PRIVATE_URL;
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ url: url })
+    };
 };
